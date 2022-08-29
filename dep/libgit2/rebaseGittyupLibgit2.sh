e24c7fbdb0665ca105996dbb2e5f9071792cac07
Martin Marmsoler <martin.marmsoler@gmail.com>
2022-08-29 14:43:14 +0200

    update merge script

--- dep/libgit2/rebaseGittyupLibgit2.sh
+++ dep/libgit2/rebaseGittyupLibgit2.sh
@@ -1,10 +1,17 @@
-declare -a branches=("origin/fetch_annotated_tags" "origin/blame_abort" "origin/disableRenameDetection" "origin/checkout_deletion_notification" "origin/diff_checkout" "origin/fix_push_callback_issues" "origin/disable_mmap" "origin/context_line_accessor´" "origin/disableRenameDetection" "origin/submodule" "origin/hash" "origin/callback_connect_disconnect" "origin/blame_buffer" "origin/libgit2_includes_public")
+declare -a branches=("fetch_annotated_tags" "blame_abort" "disableRenameDetection" "checkout_deletion_notification" "diff_checkout" "fix_push_callback_issues" "disable_mmap" "context_line_accessor´" "disableRenameDetection" "submodule" "hash" "callback_connect_disconnect" "blame_buffer" "libgit2_includes_public")
 
 cd libgit2
 
+git remote remove origin
+git remote remove upstream
+git remote add origin git@github.com:Murmele/libgit2.git
+git remote add upstream https://github.com/libgit2/libgit2.git
+
+git fetch --all
+
 for branch in ${branches[@]}; do
 	echo $branch
-	git checkout $branch
+	git checkout -B $branch "origin/$branch"
 	git rebase upstream/main
 done
 
@@ -14,5 +21,7 @@ git reset --hard upstream/main
 for branch in ${branches[@]}; do
 	git merge --no-edit $branch
 done
+
+git push --force origin
 
 echo "Script finished. Check if all branches are rebased correctly and push them to origin!"
