310cbdf72ec5b3ed38bc0171fd4acdc214fa130c
Martin Marmsoler <martin.marmsoler@gmail.com>
2023-02-15 19:30:34 +0100

    Description: Enable debug message with cmake Reason: So it can be changed without touching the code Files: CMakeLists.txt and Debug.h

--- src/util/Debug.h
+++ src/util/Debug.h
@@ -3,33 +3,22 @@
 
 #include <QDebug>
 
-#define DEBUG 1
-#define DEBUG_REFRESH 1
-
-#if DEBUG == 0
+#ifdef DEBUG
 
 #define Debug(x)                                                               \
   do {                                                                         \
     qDebug() << x;                                                             \
   } while (false)
-
 #else
-
 #define Debug(x)
-
 #endif
 
-#if DEBUG_REFRESH == 1
-
+#ifdef DEBUG_REFRESH
 #define DebugRefresh(x)                                                        \
   do {                                                                         \
     qDebug() << Q_FUNC_INFO << QStringLiteral(": ") << x;                      \
   } while (false)
-
 #else
-
 #define DebugRefresh(x)
-
 #endif
-
 #endif // DEBUG_H
