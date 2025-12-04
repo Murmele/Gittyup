914d6c92b572a1844830a1fb88a26a111164cc77
Martin Marmsoler <martin.marmsoler@gmail.com>
2025-12-04 16:47:56 +0100

    In the metainfo all description titles must be paragraphs <p>
    Reason: Otherwise the linter fails

--- cmake/generate_appdata.cmake
+++ cmake/generate_appdata.cmake
@@ -11,8 +11,8 @@
 file(READ "${CHANGELOG_HTML}" HTML_CHANGELOGS)
 # it is not allowed to have multiple texts without being in an environment
 # So the description will be used for it
-string(REGEX REPLACE "<p>([^<]*)<\\/p>" "\\1" RELEASES ${HTML_CHANGELOGS}) # remove paragraph environment
-string(REGEX REPLACE "<h4>([A-Za-z0-9]*)<\\/h4>" "<p>\\1</p>" RELEASES ${RELEASES}) # h4 is unknow to appdata so change it to a paragraph environment
+#string(REGEX REPLACE "<p>([^<]*)<\\/p>" "\\1" RELEASES ${HTML_CHANGELOGS}) # remove paragraph environment. In the metainfo all description titles must be paragraphs <p>
+string(REGEX REPLACE "<h4>([A-Za-z0-9]*)<\\/h4>" "<p>\\1</p>" RELEASES ${HTML_CHANGELOGS}) # h4 is unknow to appdata so change it to a paragraph environment
 string(REPLACE "\n" "\n\t" RELEASES ${RELEASES}) # add tabulator
 # For Dev Version "vX.X.X - <current date> (DEV)" can be used to show in the changelog the current progress
 string(REGEX REPLACE "<h3>(v[1-9]\\.[0-9]\\.[0-9]|vX\\.X\\.X) - ([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9])( \\(DEV\\))?<\\/h3>" "<release version='\\1' date='\\2'>\n\t<description>" RELEASES ${RELEASES})
